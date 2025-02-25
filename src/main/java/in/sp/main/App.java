b3e618b30c8ef790c9a9b4460935a5b881fde857
akib1830 <mullaakib4@gmail.com>
2025-02-25 15:47:08 +0530

    indian dev

--- src/main/java/in/sp/main/App.java
+++ src/main/java/in/sp/main/App.java
@@ -24,7 +24,7 @@ public class App
     		System.out.println("-------- -----------");
     		System.out.println("====================");
     		
-    		System.out.println("--test1--");
+    		System.out.println("India update");
     	}
     }
 
